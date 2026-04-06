317e79d51c10953bd348d0a4dfa6ef19ebe80b7c
Lukas <ldiazc3@soy.sena.edu.co>
2026-04-06 14:05:51 -0500

    update: cambio de inserciones
    quito la inserción de grupos y dejo la inserción de inscripción en NULL por defecto...

--- baseDatos/Data.sql
+++ baseDatos/Data.sql
@@ -30,17 +30,10 @@ INSERT INTO Aprendiz VALUES
 (5, 'Diego Fernandez', 23, 'diegdx@outlook.com', 3049876543, 'Diego Fernandez', 'diegdx@outlook.com', 'pass789', 'Aprendiz', 'M', 26702),
 (6, 'Valentina Lopez', 20, 'vale2d9@outlook.com', 3054567890, 'Valentina Lopez', 'vale2d9@outlook.com', 'clave321', 'Aprendiz', 'F', 26703);
 
-       
--- Insertar en Grupos 
-INSERT INTO Grupos VALUES
-(NULL, 'Grupo 1'),
-(NULL, 'Grupo 2'),
-(NULL, 'Grupo 3')
-
 -- Insertar en Inscripcion (Depende de Aprendiz y Evento)
-INSERT INTO Inscripciones VALUES (9001, '2024-04-01', 'Presencial', 1, 501, 1);
-INSERT INTO Inscripciones VALUES (9002, '2024-04-02', 'Virtual', 2, 501, 2);
-INSERT INTO Inscripciones VALUES (9003, '2024-04-05', 'Presencial', 3, 502, 3)
+INSERT INTO Inscripciones VALUES (9001, '2024-04-01', 'Presencial', 1, 501, NULL);
+INSERT INTO Inscripciones VALUES (9002, '2024-04-02', 'Virtual', 2, 501, NULL);
+INSERT INTO Inscripciones VALUES (9003, '2024-04-05', 'Presencial', 3, 502, NULL);
 
 -- Hacer idGrupo opcional en Inscripciones (no elimina FK, solo permite NULL)
 -- Ejecutar en entorno de pruebas antes de producción.
